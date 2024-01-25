57acfd982d36e57f35bacf2c93b828ca714f7249
Saydum <saydum.it5@gmail.com>
2024-01-26 02:38:28 +0300

    init project

---
+++ src/main/java/ru/rodovayakniga/RodovayaknigaRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.rodovayakniga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodovayaknigaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodovayaknigaRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rodovayakniga/RodovayaknigaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rodovayakniga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodovayaknigaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
